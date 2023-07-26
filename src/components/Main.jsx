cf25c0b1f4c65cb9d2716ffb69a7d5aa0d60278e
Karolin Rafalski <kr2174@caa.columbia.edu>
2023-07-26 12:26:23 -0400

    done

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 
 function Main({ dogs }) {
-  console.log(dogs);
+  const getAverage = (dogs) => {
+    let sum = 0;
+    for (let dog of dogs) {
+      sum += parseInt(dog.grade);
+    }
+    return (sum / dogs.length).toFixed(2);
+  };
+  const average = getAverage(dogs);
   return (
     <main>
-      <h2>Class Average:</h2>
+      <h2>Class Average:{average}</h2>
       <h3>Notable performances and grades:</h3>
       <ul>
         {dogs.map((dog) => {
